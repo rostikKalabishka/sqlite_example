7968d009a0f5e81c5a48126bbf1c59ce5e38105e
RostikKalabishka <rostik31052002@gmail.com>
2024-01-19 15:06:22 +0200

    added change

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sqflite_example/pages/notes_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
