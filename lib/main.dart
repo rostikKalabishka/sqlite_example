baddf48b0447df9b2b5ef74be54c795294570790
RostikKalabishka <rostik31052002@gmail.com>
2024-01-19 15:05:27 +0200

    added example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_example/pages/notes_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,64 +7,22 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  static String title = 'Notes SQLite';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: title,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.blueGrey.shade900,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const NotesPage(),
     );
   }
 }
---
+++ lib/pages/notes_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:sqflite_example/db/note_database.dart';
+import 'package:sqflite_example/pages/edit_note_page.dart';
+import 'package:sqflite_example/pages/note_detail_page.dart';
+
+import '../model/note.dart';
+
+import '../widget/note_card_widget.dart';
+
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key});
+
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  late List<Note> notes;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    refreshNotes();
+  }
+
+  @override
+  void dispose() {
+    NotesDatabase.instance.close();
+
+    super.dispose();
+  }
+
+  Future refreshNotes() async {
+    setState(() => isLoading = true);
+
+    notes = await NotesDatabase.instance.readAllNotes();
+
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Notes',
+            style: TextStyle(fontSize: 24),
+          ),
+          actions: const [Icon(Icons.search), SizedBox(width: 12)],
+        ),
+        body: Center(
+          child: isLoading
+              ? const CircularProgressIndicator()
+              : notes.isEmpty
+                  ? const Text(
+                      'No Notes',
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    )
+                  : buildNotes(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
+          child: const Icon(Icons.add),
+          onPressed: () async {
+            await Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const AddEditNotePage()),
+            );
+
+            refreshNotes();
+          },
+        ),
+      );
+  Widget buildNotes() => StaggeredGrid.count(
+      // itemCount: notes.length,
+      // staggeredTileBuilder: (index) => StaggeredTile.fit(2),
+      crossAxisCount: 2,
+      mainAxisSpacing: 2,
+      crossAxisSpacing: 2,
+      children: List.generate(
+        notes.length,
+        (index) {
+          final note = notes[index];
+
+          return StaggeredGridTile.fit(
+            crossAxisCellCount: 1,
+            child: GestureDetector(
+              onTap: () async {
+                await Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => NoteDetailPage(noteId: note.id!),
+                ));
+
+                refreshNotes();
+              },
+              child: NoteCardWidget(note: note, index: index),
+            ),
+          );
+        },
+      ));
+
+  // Widget buildNotes() => StaggeredGridView.countBuilder(
+  //       padding: const EdgeInsets.all(8),
+  //       itemCount: notes.length,
+  //       staggeredTileBuilder: (index) => StaggeredTile.fit(2),
+  //       crossAxisCount: 4,
+  //       mainAxisSpacing: 4,
+  //       crossAxisSpacing: 4,
+  //       itemBuilder: (context, index) {
+  //         final note = notes[index];
+
+  //         return GestureDetector(
+  //           onTap: () async {
+  //             await Navigator.of(context).push(MaterialPageRoute(
+  //               builder: (context) => NoteDetailPage(noteId: note.id!),
+  //             ));
+
+  //             refreshNotes();
+  //           },
+  //           child: NoteCardWidget(note: note, index: index),
+  //         );
+  //       },
+  //     );
+}
